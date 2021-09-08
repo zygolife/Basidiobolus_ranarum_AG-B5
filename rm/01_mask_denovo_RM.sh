28baf316d93b40b1df6a88df0e658e9dabda32a0
Jason Stajich <jasonstajich.phd@gmail.com>
2021-09-08 11:24:13 -0700

    support multiple

--- rm/01_mask_denovo_RM.sh
+++ rm/01_mask_denovo_RM.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-#SBATCH -p batch,intel -n 24 --mem 96gb --out RM.%A.log -N 1
+#SBATCH -p batch,intel -n 24 --mem 96gb --out RM.%a.log -N 1
 module unload miniconda3
 module unload miniconda2
 module load RepeatModeler
@@ -9,7 +9,16 @@ else
     CPUS=${SLURM_CPUS_ON_NODE}
 fi
 
-NAME=Basidiobolus_ranarum_AG-B5.v1.sorted.fasta
+N=${SLURM_ARRAY_TASK_ID}
+if [ ! $N ]; then
+    N=$1
+    if [ ! $N ]; then
+        echo "need to provide a number by --array or cmdline"
+        exit
+    fi
+fi
+
+NAME=$(ls *.sorted.fasta | sed -n ${N}p)
 PREFIX=$(basename $NAME .sorted.fasta)
 if [ ! -f $PREFIX.nin ]; then
 	BuildDatabase -engine rmblast -name $PREFIX $NAME
